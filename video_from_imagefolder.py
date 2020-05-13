8a9f605c2fc66f9ab3d22f4180910a66acf0eafe
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2020-05-13 15:59:26 -0400

    video from image folder

--- video_from_imagefolder.py
+++ video_from_imagefolder.py
@@ -1,16 +1,17 @@
 import cv2
-import numpy as np
-import glob
+import os
 
-img_array = []
-for filename in glob.glob('folder_name/*.png'):#folder name
-    img = cv2.imread(filename)
-    height, width, layers = img.shape
-    size = (width, height)
-    img_array.append(img)
+image_folder = 'images_assignment_9_1'
+video_name = 'Video_assignment_9_01.avi'
 
-out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
 
-for i in range(len(img_array)):
-    out.write(img_array[i])
-out.release()
+video = cv2.VideoWriter(video_name, 0, 5, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
